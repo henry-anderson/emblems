a75377856e0a62733a27334a05b5224fd4e4f5fb
Henry Anderson <henryanderson1@hotmail.com>
2019-06-04 18:48:47 -0500

    update

--- services/spigotbukkit/downloads.php
+++ services/spigotbukkit/downloads.php
@@ -2,6 +2,16 @@
 header("Content-Type: image/svg+xml");
 header("Cache-Control: max-age=0");
 
+$spigot_id = $_GET('spigot');
+$bukkit_id = $_GET('bukkit');
+$color = $_GET('color');
+$name = ($_GET('name') != null) ? $_GET('name') : "downloads";
+
+echo "Spigot: " . $spigot_id;
+echo "Bukkit: " . $bukkit_id;
+echo "Color: " . $color;
+echo "Name: " . $name;
+
 function get_spigot_downloads() {
     $page = file_get_contents("https://api.spiget.org/v2/resources/3829");
     $firstpart = explode("\"downloads\": ", $page)[1];
@@ -10,7 +20,7 @@ function get_spigot_downloads() {
 }
 
 function get_bukkit_downloads() {
-    $page = file_get_contents("https://dev.bukkit.org/projects/pingapi");
+    $page = file_get_contents("https://dev.bukkit.org/projects/89296");
     $firstpart = explode("<div class=\"info-data\">", $page)[4];
     $downloads = explode("</div>", $firstpart)[0];
     return intval(str_replace(",", "", $downloads));
